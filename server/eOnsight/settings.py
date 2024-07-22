d79e96c73204810151bdc7c55bd0759294d87387
Saintkops <erwanchevrier6@gmail.com>
2024-07-23 01:14:57 +0200

    feature: Pagination added

--- server/eOnsight/settings.py
+++ server/eOnsight/settings.py
@@ -132,6 +132,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#add pagination to API
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10, 
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
