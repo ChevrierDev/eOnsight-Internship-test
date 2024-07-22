f98394c373e7cbf890c862d666286c5283159d86
Saintkops <erwanchevrier6@gmail.com>
2024-07-23 01:41:13 +0200

    feature: allow cors all origin added

--- server/eOnsight/settings.py
+++ server/eOnsight/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'django_filters',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'eOnsight.urls'
 
