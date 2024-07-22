c552f26d5c457feefe0c2cec3232ad185d4bd705
Saintkops <erwanchevrier6@gmail.com>
2024-07-23 01:33:47 +0200

    feature : Filter added

--- server/bridgesAPI/views.py
+++ server/bridgesAPI/views.py
@@ -3,13 +3,14 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from .models import Bridge
 from .serializers import BridgeSerializers  
+from .filters import BridgeFilter  
 
 # Retrieve Bridge list data
 class BridgeList(generics.ListCreateAPIView):
     queryset = Bridge.objects.all()
     serializer_class = BridgeSerializers
-    filter_backends = [DjangoFilterBackend]  
-    filterset_fields = ['status', 'inspection_date']
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BridgeFilter  # Utiliser le filtre personnalisé
 
 # Retrieve Bridge details, update, delete or post Bridge
 class BridgeDetails(generics.RetrieveUpdateDestroyAPIView):
