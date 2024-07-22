842b0fe7550cda87170dad546aa748dcd7a92b24
Saintkops <erwanchevrier6@gmail.com>
2024-07-22 21:25:29 +0200

    feature: serializers added

--- server/bridgesAPI/serializers.py
+++ server/bridgesAPI/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Bridge
 
+#Serialize data from Bridge model
 class BridgeSerializers(serializers.ModelSerializer):
     class Meta:
         model = Bridge,
