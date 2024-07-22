930955de935a1cdcfc07b926ee3cd693a8789987
Saintkops <erwanchevrier6@gmail.com>
2024-07-23 00:20:12 +0200

    update: delete useless code

--- server/bridgesAPI/views.py
+++ server/bridgesAPI/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
 from rest_framework import generics
-from rest_framework.response import Response
 from .models import Bridge
 from .serializers import BridgeSerializers  
-from rest_framework.views import APIView
-from rest_framework import status
 
 #retrieve Bridge list data
 class BridgeList(generics.ListCreateAPIView):
