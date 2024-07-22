88ce0d5618e89366e3ca24f4b3ee1b36db5437e5
Saintkops <erwanchevrier6@gmail.com>
2024-07-22 22:49:22 +0200

    Update: Add status option validation

--- server/bridgesAPI/serializers.py
+++ server/bridgesAPI/serializers.py
@@ -5,4 +5,17 @@ from .models import Bridge
 class BridgeSerializers(serializers.ModelSerializer):
     class Meta:
         model = Bridge,
+        STATUS_OPTIONS = ['Good', 'Fair', 'Poor', 'Bad']
         fields = ('__all__')
+        
+    def validate_status(self, value):
+        """
+        Validate the status field to ensure it contains a valid value.
+
+        Returns:
+            str: The validated value.
+        """
+        # if values in status_option return value
+        if value not in self.STATUS_CHOICES:
+            raise serializers.ValidationError("Only options 'Good', 'Fair', 'Poor', or 'Bad' are allowed.")
+        return value
--- server/bridgesAPI/urls.py
+++ server/bridgesAPI/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path, include
+from django.urls import path
+from .views import BridgeList, BridgeDetails
 
 urlpatterns = [
+    path('bridges/',BridgeList.as_view() ),
+    path('bridges/<int:pk>/', BridgeDetails.as_view() )
 
 ]
