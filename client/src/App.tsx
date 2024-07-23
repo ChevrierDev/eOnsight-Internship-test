84bca1cb1155f134477ae2ad3441d041bc146b94
Saintkops <erwanchevrier6@gmail.com>
2024-07-23 18:16:51 +0200

    feat(client): structure folder done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
-import './App.css'
-import './output.css'
+import './styles/output.css'
 
 function App() {
   return (
     <>
       <div>
-        <h1 className='text-green-700 text-4xl'>Hello world</h1>
+        <h1 className='text-green-700 text-4xl'>Hello worldsdsdfezfe</h1>
       </div>
     </>
   )
 }
 
 export default App
+
