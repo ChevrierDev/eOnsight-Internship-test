87b589a5ea0004767eacecf09af20f4e3c486f70
Saintkops <erwanchevrier6@gmail.com>
2024-07-23 18:38:37 +0200

    feature(client): object types added

---
+++ client/src/types/index.tsx
@@ -0,0 +1,18 @@
+//define types for Bridges object
+export interface Bridge {
+    readonly id: number;
+    name: string;
+    location: GeoLocation;
+    inspection_date: Date;
+    status: Status;
+    traffic_load: number;
+}
+  
+//Define types for geo-spatial locations
+export interface GeoLocation {
+    latitude: number;
+    longitude: number;
+}
+
+//Define Alias for status only allowed strings : "Good" | "Fair" | "Poor" | "Bad"
+export type Status = "Good" | "Fair" | "Poor" | "Bad"
