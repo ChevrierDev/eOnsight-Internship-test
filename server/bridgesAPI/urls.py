99d1523abd15e69c266ebd3806e486dd5b03fa8c
Saintkops <erwanchevrier6@gmail.com>
2024-07-26 15:26:35 +0200

    feat(server): add search API

--- server/bridgesAPI/urls.py
+++ server/bridgesAPI/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BridgeList, BridgeDetails
+from .views import BridgeList, BridgeDetails, BridgeSearchByName
 
 urlpatterns = [
     path('bridges/', BridgeList.as_view()),
     path('bridges/<int:pk>/', BridgeDetails.as_view()),
+    path('bridges/search/', BridgeSearchByName.as_view()), 
 ]
